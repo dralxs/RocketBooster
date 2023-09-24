db8f170b576298a756bcdcf6db531cd5fea0b99b
dralxs <dr.ales@protonmail.com>
2023-09-24 14:01:33 +0200

    [feat] playerrocket / camera / map

---
+++ Game1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
+
+
+namespace RocketBooster
+{
+    public class Game1 : Game
+    {
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        TiledMap _tiledMap;
+        TiledMapRenderer _tiledMapRenderer;
+        private OrthographicCamera _camera;
+        private SpriteFont font;
+
+        private Rocket playerRocket;
+
+        Vector2 screenCenter;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // Instanciate the camera
+            var viewportadapter = new BoxingViewportAdapter(Window, GraphicsDevice, 1280, 720);
+            _camera = new OrthographicCamera(viewportadapter);
+            // Instanciate the main rocket
+            playerRocket = new Rocket(_camera);
+            // Get the center of the screen
+            Viewport viewport = _graphics.GraphicsDevice.Viewport;
+            screenCenter =  new Vector2(viewport.Width / 2f, viewport.Height / 2f);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            // Texture rocket
+            playerRocket.LoadContent(this.Content);
+            //asteroidTexture = Content.Load<Texture2D>("asteroid1");
+            _tiledMap = Content.Load<TiledMap>("rocketboostermap");
+            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            // Debug Panel
+            font = Content.Load<SpriteFont>("CameraPosition");
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _tiledMapRenderer.Update(gameTime);
+            // Update the camera 
+            playerRocket.MoveCamera(gameTime);
+            _camera.LookAt(playerRocket._cameraPosition);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Start of the drawing 
+            _spriteBatch.Begin();
+
+            // Draw the rocket 
+            playerRocket.Draw(_spriteBatch, this.screenCenter);
+            _spriteBatch.DrawString(font, "CameraPosition : " + playerRocket._cameraPosition, new Vector2(0,0), Color.Black);
+
+            // End of the drawing
+            _spriteBatch.End();
+
+            _tiledMapRenderer.Draw(_camera.GetViewMatrix());
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Rocket.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonoGame.Extended.ViewportAdapters;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System.Reflection.Metadata;
+using Microsoft.Xna.Framework.Content;
+using MonoGame.Extended.Screens;
+using System.Diagnostics;
+
+namespace RocketBooster
+{
+    internal class Rocket
+    {
+        Texture2D rocketTexture;
+        private OrthographicCamera _camera;   
+        private int speed;
+        private int damage;
+        private float angle;
+        private Vector2 imageCenter;
+
+        public Vector2 _cameraPosition;
+
+        // PUBLIC FUNCTION //
+
+        public Rocket(OrthographicCamera camera)
+        {
+            _camera = camera;
+            _cameraPosition = new Vector2(0, 0);
+            speed = 5;
+            damage = 1;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            this.rocketTexture = content.Load<Texture2D>("rocket1");
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 screenCenter)
+        {
+            this.imageCenter = new Vector2(rocketTexture.Width / 2f, rocketTexture.Height / 2f);
+            spriteBatch.Draw(rocketTexture, screenCenter, null, Color.White, this.angle, imageCenter, 1f, SpriteEffects.None, 0f);
+        }
+
+        public void MoveCamera(GameTime gameTime)
+        {
+            var speed = 200;
+            var seconds = gameTime.GetElapsedSeconds();
+            var movementDirection = GetMovementDirection();
+            this._cameraPosition += speed * movementDirection * seconds;
+        }
+
+        // PRIVATE FUNCTION //
+        private const float RotationSpeed = 0.1f;
+
+        private Vector2 GetMovementDirection()
+        {
+            var movementDirection = Vector2.Zero;
+            var state = Keyboard.GetState();
+
+            // Handle movement
+            if (state.IsKeyDown(Keys.Down))
+            {
+                movementDirection += Vector2.UnitY;
+            }
+            if (state.IsKeyDown(Keys.Up))
+            {
+                movementDirection -= Vector2.UnitY;
+            }
+            if (state.IsKeyDown(Keys.Left))
+            {
+                movementDirection -= Vector2.UnitX;
+            }
+            if (state.IsKeyDown(Keys.Right))
+            {
+                movementDirection += Vector2.UnitX;
+            }
+
+            // Calculate the target angle based on the movement direction
+            if (movementDirection != Vector2.Zero)
+            {
+                float targetAngle = MathF.Atan2(movementDirection.X, -movementDirection.Y);
+                Debug.WriteLine("Target Angle: " + targetAngle);
+
+                // Smoothly transition the current angle towards the target angle
+                float deltaAngle = targetAngle - this.angle;
+                if (MathF.Abs(deltaAngle) > MathF.PI)
+                {
+                    deltaAngle += MathF.Sign(deltaAngle) * 2 * MathF.PI;
+                }
+                this.angle += deltaAngle * RotationSpeed;
+
+                // Normalize the movement direction
+                movementDirection.Normalize();
+            }
+
+            return movementDirection;
+        }
+
+    }
+} 
+
